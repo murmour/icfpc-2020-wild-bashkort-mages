e0b1b0504b7007649fe0df9db8b775be171c87c4
murmour <mmouratov@gmail.com>
2020-07-19 19:48:36 +0500

    MAGIC

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-eval "$(opam env)"
-ocamlfind opt -linkpkg -package curl app/main.ml -o app/app
+# MAGIC!
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-eval "$(opam env)"
 app/app "$@" || echo "run error code: $?"
